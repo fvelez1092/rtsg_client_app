c674f7f237300035f47f63f45f5b9770fb258d2b
fvelez1092 <46549034+fvelez1092@users.noreply.github.com>
2026-09-05 15:52:58 -0500

    refactor: usar TripController como controlador unico del viaje

--- lib/presentation/pages/trip/trip_binding.dart
+++ lib/presentation/pages/trip/trip_binding.dart
@@ -1,4 +1,3 @@
-import 'package:app_rtsg_client/application/home2_controller.dart';
 import 'package:app_rtsg_client/application/trip_controller.dart';
 import 'package:app_rtsg_client/data/services/trip_simulator_service.dart';
 import 'package:get/get.dart';
@@ -7,7 +6,6 @@ class TripBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<TripSimulatorService>(() => TripSimulatorService());
-    Get.lazyPut<Home2Controller>(() => Home2Controller());
     Get.lazyPut<TripController>(() => TripController());
   }
 }
