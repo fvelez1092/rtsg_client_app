c86fd2cb4006829c2d99835059d9ba670d5ca394
fvelez1092 <46549034+fvelez1092@users.noreply.github.com>
2026-09-05 16:12:46 -0500

    refactor: usar dashboard para las rutas principales

--- lib/routes/rtsg_routes.dart
+++ lib/routes/rtsg_routes.dart
@@ -1,7 +1,5 @@
 import 'package:app_rtsg_client/presentation/dashboard/dashboard_binding.dart';
 import 'package:app_rtsg_client/presentation/dashboard/dashboard_page.dart';
-import 'package:app_rtsg_client/presentation/pages/account/account_page.dart';
-import 'package:app_rtsg_client/presentation/pages/activity/activity_page.dart';
 import 'package:app_rtsg_client/presentation/pages/auth/forgot_password/forgot_password_binding.dart';
 import 'package:app_rtsg_client/presentation/pages/auth/forgot_password/forgot_password_page.dart';
 import 'package:app_rtsg_client/presentation/pages/auth/login/login_binding.dart';
@@ -9,8 +7,6 @@ import 'package:app_rtsg_client/presentation/pages/auth/login/login_page.dart';
 import 'package:app_rtsg_client/presentation/pages/auth/register/register_binding.dart';
 import 'package:app_rtsg_client/presentation/pages/auth/register/register_page.dart';
 import 'package:app_rtsg_client/presentation/pages/delivery/delivery_page.dart';
-import 'package:app_rtsg_client/presentation/pages/home/home_binding.dart';
-import 'package:app_rtsg_client/presentation/pages/home/home_page.dart';
 import 'package:app_rtsg_client/presentation/pages/partners/partner_detail_page.dart';
 import 'package:app_rtsg_client/presentation/pages/partners/partners_page.dart';
 import 'package:app_rtsg_client/presentation/pages/profile/profile_binding.dart';
@@ -20,7 +16,6 @@ import 'package:app_rtsg_client/presentation/pages/splash/splash_binding.dart';
 import 'package:app_rtsg_client/presentation/pages/splash/splash_page.dart';
 import 'package:app_rtsg_client/presentation/pages/trip/trip_binding.dart';
 import 'package:app_rtsg_client/presentation/pages/trip/trip_page.dart';
-import 'package:app_rtsg_client/presentation/pages/wallet/wallet_page.dart';
 import 'package:get/get.dart';
 
 abstract class AppPages {
@@ -71,26 +66,32 @@ abstract class AppPages {
       name: AppRoutes.PARTNER_DETAIL,
       page: () => const PartnerDetailPage(),
     ),
+
+    // Las cuatro rutas principales comparten el mismo shell de Dashboard.
+    // DashboardController selecciona la tab correcta a partir de la ruta.
+    GetPage(
+      name: AppRoutes.HOME,
+      page: () => const DashboardPage(),
+      binding: DashboardBinding(),
+    ),
     GetPage(
       name: AppRoutes.ACTIVITY,
-      page: () => const ActivityPage(),
+      page: () => const DashboardPage(),
+      binding: DashboardBinding(),
     ),
     GetPage(
       name: AppRoutes.WALLET,
-      page: () => const WalletPage(),
+      page: () => const DashboardPage(),
+      binding: DashboardBinding(),
     ),
     GetPage(
       name: AppRoutes.ACCOUNT,
-      page: () => const AccountPage(),
-    ),
-    GetPage(
-      name: AppRoutes.HOME,
-      page: () => HomePage(),
-      binding: HomeBinding(),
+      page: () => const DashboardPage(),
+      binding: DashboardBinding(),
     ),
     GetPage(
       name: AppRoutes.DASHBOARD,
-      page: () => DashboardPage(),
+      page: () => const DashboardPage(),
       binding: DashboardBinding(),
     ),
   ];
