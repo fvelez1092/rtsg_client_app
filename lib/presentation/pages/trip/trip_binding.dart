46aed9da2401e45d07f19f24cc115d57eb12396d
fvelez1092 <46549034+fvelez1092@users.noreply.github.com>
2026-09-09 13:49:21 -0500

    Conectar TripController con servicio API real

--- lib/presentation/pages/trip/trip_binding.dart
+++ lib/presentation/pages/trip/trip_binding.dart
@@ -1,11 +1,11 @@
 import 'package:app_rtsg_client/application/trip_controller.dart';
-import 'package:app_rtsg_client/data/services/trip_simulator_service.dart';
+import 'package:app_rtsg_client/data/services/trip_service.dart';
 import 'package:get/get.dart';
 
 class TripBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<TripSimulatorService>(() => TripSimulatorService());
+    Get.lazyPut<TripService>(() => TripService());
     Get.lazyPut<TripController>(() => TripController());
   }
 }
