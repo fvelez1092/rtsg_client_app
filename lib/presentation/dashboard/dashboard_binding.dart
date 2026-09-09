3ee82e75ab10825bff831b1a04d104d967549d48
fvelez1092 <46549034+fvelez1092@users.noreply.github.com>
2026-09-09 13:48:37 -0500

    Registrar servicios de carreras en Dashboard

--- lib/presentation/dashboard/dashboard_binding.dart
+++ lib/presentation/dashboard/dashboard_binding.dart
@@ -1,8 +1,10 @@
 import 'package:app_rtsg_client/application/dashboard_controller.dart';
 import 'package:app_rtsg_client/application/home_content_controller.dart';
 import 'package:app_rtsg_client/application/home_controller.dart';
+import 'package:app_rtsg_client/application/trips_controller.dart';
 import 'package:app_rtsg_client/data/services/partners_service.dart';
 import 'package:app_rtsg_client/data/services/publicidad_service.dart';
+import 'package:app_rtsg_client/data/services/trip_service.dart';
 import 'package:get/get.dart';
 
 class DashboardBinding extends Bindings {
@@ -12,6 +14,11 @@ class DashboardBinding extends Bindings {
 
     Get.lazyPut<PartnersService>(() => PartnersService(), fenix: true);
     Get.lazyPut<PublicidadService>(() => PublicidadService(), fenix: true);
+    Get.lazyPut<TripService>(() => TripService(), fenix: true);
+    Get.lazyPut<TripsController>(
+      () => TripsController(Get.find<TripService>()),
+      fenix: true,
+    );
 
     Get.lazyPut<HomeContentController>(
       () => HomeContentController(
